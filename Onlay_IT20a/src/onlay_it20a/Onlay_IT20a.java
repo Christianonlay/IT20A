a9219d51d2708c1253dbc0faf0cc77d6812e1d45
Christianonlay <onlaychristian99@gmail.com>
2025-09-04 18:44:57 +0800

    Initialized folder

---
+++ Onlay_IT20a/src/onlay_it20a/Onlay_IT20a.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package onlay_it20a;
+
+/**
+ *
+ * @author bmang
+ */
+public class Onlay_IT20a {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
